4f3b1eba8a1a214aa0b74b708198911c1631646c
leesangeok <wyzmqkr11@naver.com>
2024-11-25 11:24:18 +0900

    10 level

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,24 @@
+//plugins {
+//    id("java")
+//}
+//
+//group = "com.ll"
+//version = "1.0-SNAPSHOT"
+//
+//repositories {
+//    mavenCentral()
+//}
+//
+//dependencies {
+//    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
+//    implementation 'com.google.code.gson:gson:2.9.0'
+//
+//}
+//
+//tasks.test {
+//    useJUnitPlatform()
+//}
 plugins {
     id("java")
 }
@@ -6,12 +27,13 @@ group = "com.ll"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    mavenCentral() // Maven Central 저장소
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.code.gson:gson:2.9.0") // Kotlin DSL에 맞는 문법으로 수정
 }
 
 tasks.test {
